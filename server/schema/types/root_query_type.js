19838e7745f3caceace3c5e5b3cbce05d919e3a2
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-20 16:41:08 -0500

    created graphql data_type

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql');
 const { GraphQLObjectType, GraphQLID } = graphql;
 const UserType = require('./user_type');
+const DataType = require('./data_type');
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -9,6 +10,12 @@ const RootQueryType = new GraphQLObjectType({
       type: UserType,
       resolve(parentValue, args, req) {
         return req.user;
+      }
+    },
+    data: {
+      type: DataType,
+      resolve(parentValue, args, req) {
+        return {};
       }
     }
   }
