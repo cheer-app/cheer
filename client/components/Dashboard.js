1110f6a043b2b412a6b4c32e21f6552f87ec6b85
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-03-20 15:40:40 -0500

    cleared out old D3 charts

--- client/components/Dashboard.js
+++ client/components/Dashboard.js
@@ -1,72 +1,9 @@
 import React, { Component } from 'react';
-// import Bubble from './Bubble';
-import WordCloud from 'react-d3-cloud';
-
-const data = [
-  { text: 'Hey', value: 1000 },
-  { text: 'lol', value: 200 },
-  { text: 'first impression', value: 800 },
-  { text: 'very cool', value: 1000000 },
-  { text: 'duck', value: 10 },
-];
-
-const fontSizeMapper = word => Math.log2(word.value) * 5;
-const rotate = word => word.value % 360;
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <div id="nav">
-          <h1>Cheer App</h1>
-          <p>
-            Explore emloyee wellness over time: <span id="year-val" />
-            <input id="year" type="range" step="1" />
-          </p>
-          <p>
-            Choose between keywords or entities.
-            <input type="radio" name="data-type" value="keyword" checked />
-            <label>Keywords</label>
-            <input type="radio" name="data-type" value="entities" />
-            <label>Entities</label>
-          </p>
-          <p>Click on a word to see its trends over time.</p>
-        </div>
-
-        <WordCloud
-          data={data}
-          fontSizeMapper={fontSizeMapper}
-          // rotate={rotate}
-        />
-
-        {/* <Bubble /> */}
-
-        {/* <section className="container">
-          <div className="chart-container">
-          <svg
-            version="1.1"
-            baseProfile="full"
-            xmlns="http://www.w3.org/2000/svg"
-            id="map">
-          </svg>
-        </div>
-        <div className="chart-container right">
-          <svg
-            version="1.1"
-            baseProfile="full"
-            xmlns="http://www.w3.org/2000/svg"
-            id="pie">
-          </svg>
-          <svg
-            version="1.1"
-            baseProfile="full"
-            xmlns="http://www.w3.org/2000/svg"
-            id="bar">
-          </svg>
-        </div>
-        </section> */}
-        <div className="tooltip" />
-      </div>
+      <div>LOGGED IN</div>
     );
   }
 }
