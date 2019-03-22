562de892fe6a7b4135864c05ce8e7f7cf82e1026
Jake H <43487010+greatheights82@users.noreply.github.com>
2019-03-22 11:11:54 -0500

    Material ui (#19)
    * material-ui applied to login form
    
    * started implementing material-ui header.
    
    * header materialized. still need to fix account menu

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,55 +1,118 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { Link } from 'react-router';
-import query from '../queries/CurrentUser';
-import mutation from '../mutations/Logout';
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import { Link } from 'react-router'
+import query from '../queries/CurrentUser'
+import mutation from '../mutations/Logout'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import AccountCircle from '@material-ui/icons/AccountCircle'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 5,
+  },
+}
 
 class Header extends Component {
-  onLogoutClick() {
-    this.props.mutate({
-      refetchQueries: [{ query }]
-    });
+  constructor() {
+    super()
+    this.state = {
+      anchorEl: null,
+    }
+    this.onLogoutClick = this.onLogoutClick.bind(this)
+    this.handleMenu = this.handleMenu.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
-  renderButtons() {
-    const { loading, user } = this.props.data;
+  onLogoutClick() {
+    this.props.mutate({
+      refetchQueries: [{ query }],
+    })
+    this.handleClose()
+  }
 
-    if (loading) { return <div />; }
+  handleMenu(event) {
+    this.setState({ anchorEl: event.currentTarget })
+  }
 
-    if (user) {
-      return (
-        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
-      );
-    } else {
-      return (
-        <div>
-          <li>
-            <Link to="/signup">Signup</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </div>
-      );
-    }
+  handleClose() {
+    this.setState({ anchorEl: null })
   }
 
   render() {
+    const { classes } = this.props
+    const { loading, user } = this.props.data
+    const { anchorEl } = this.state
+    const open = Boolean(anchorEl)
+
+    if (loading) {
+      return <div />
+    }
+
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo left">
-            Home
-          </Link>
-          <ul className="right">
-            {this.renderButtons()}
-          </ul>
-        </div>
-      </nav>
-    );
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Cheer
+            </Typography>
+            {!user ? (
+              <Button component={Link} to="/login" color="inherit">
+                Login
+              </Button>
+            ) : (
+              <div>
+                <IconButton
+                  onClick={this.handleMenu.bind(this)}
+                  className={classes.menuButton}
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.onLogoutClick}>Logout</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
   }
 }
 
-export default graphql(mutation)(
-  graphql(query)(Header)
-);
+export default withStyles(styles)(graphql(mutation)(graphql(query)(Header)))
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import AuthForm from './AuthForm';
-import mutation from '../mutations/Login';
-import { graphql } from 'react-apollo';
-import query from '../queries/CurrentUser';
-import { hashHistory } from 'react-router';
+import React, { Component } from 'react'
+import AuthForm from './AuthForm'
+import mutation from '../mutations/Login'
+import { graphql } from 'react-apollo'
+import query from '../queries/CurrentUser'
+import { hashHistory } from 'react-router'
 
 class LoginForm extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { errors: [] };
+    this.state = { errors: [] }
   }
 
   componentWillUpdate(nextProps) {
@@ -18,33 +18,32 @@ class LoginForm extends Component {
     // when the component rerenders
     if (!this.props.data.user && nextProps.data.user) {
       // redirect to dashboard!!!!
-      hashHistory.push('/dashboard');
+      hashHistory.push('/dashboard')
     }
   }
 
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query }]
-    }).catch(res => {
-      const errors = res.graphQLErrors.map(error => error.message);
-      this.setState({ errors });
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }],
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(error => error.message)
+        this.setState({ errors })
+      })
   }
 
   render() {
     return (
       <div>
-        <h3>Login</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
         />
       </div>
-    );
+    )
   }
 }
 
-export default graphql(query)(
-  graphql(mutation)(LoginForm)
-);
+export default graphql(query)(graphql(mutation)(LoginForm))
