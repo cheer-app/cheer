f188bd6b35e5ecf2a9865f0d8c901873bd08802f
Jake Hebert <greatheights@gmail.com>
2019-03-22 10:30:19 -0500

    header materialized. still need to fix account menu

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,12 +3,27 @@ import { graphql } from 'react-apollo'
 import { Link } from 'react-router'
 import query from '../queries/CurrentUser'
 import mutation from '../mutations/Logout'
-
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import IconButton from '@material-ui/core/IconButton'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
+import MenuIcon from '@material-ui/icons/Menu'
 import AccountCircle from '@material-ui/icons/AccountCircle'
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 5,
+  },
+}
 
 class Header extends Component {
   onLogoutClick() {
@@ -17,64 +32,43 @@ class Header extends Component {
     })
   }
 
-  renderButtons() {
+  render() {
+    const { classes } = this.props
     const { loading, user } = this.props.data
 
     if (loading) {
       return <div />
     }
 
-    if (user) {
-      return (
-        <div>
-          <IconButton
-          // aria-owns={open ? 'menu-appbar' : undefined}
-          // aria-haspopup="true"
-          // onClick={this.handleMenu}
-          // color="inherit"
-          >
-            <AccountCircle />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            // anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={open}
-            onClose={this.handleClose}
-          >
-            <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-            <MenuItem onClick={this.handleClose}>My account</MenuItem>
-          </Menu>
-        </div>
-      )
-    } else {
-      return (
-        <Button component={Link} to="/login" color="inherit">
-          Login
-        </Button>
-      )
-    }
-  }
-
-  render() {
     return (
-      <nav>
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo left">
-            Home
-          </Link>
-          <ul className="right">{this.renderButtons()}</ul>
-        </div>
-      </nav>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Cheer
+            </Typography>
+            {user ? (
+              <IconButton
+                onClick={this.onLogoutClick.bind(this)}
+                className={classes.menuButton}
+              >
+                <AccountCircle />
+              </IconButton>
+            ) : (
+              <Button color="inherit">Login</Button>
+            )}
+          </Toolbar>
+        </AppBar>
+      </div>
     )
   }
 }
 
-export default graphql(mutation)(graphql(query)(Header))
+export default withStyles(styles)(graphql(mutation)(graphql(query)(Header)))
