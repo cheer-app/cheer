7a651dfc4836a9279e95d08d0292553ec8c37c5c
Jake Hebert <greatheights@gmail.com>
2019-03-23 15:44:24 -0500

    updateUser mutation appears to be working in graphiQl

--- server/schema/types/user_type.js
+++ server/schema/types/user_type.js
@@ -4,8 +4,8 @@ const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLBoolean } = graphql
 const UserType = new GraphQLObjectType({
   name: 'UserType',
   fields: {
-    name: { type: GraphQLString },
     id: { type: GraphQLID },
+    name: { type: GraphQLString },
     email: { type: GraphQLString },
     isAdmin: { type: GraphQLBoolean },
     slackId: { type: GraphQLString },
