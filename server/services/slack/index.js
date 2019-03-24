d269b8a69dbcbcf5709f658acd8c4e3c143a3b4e
jpoll15 <jpollack015@gmail.com>
2019-03-24 08:08:45 -0500

    configuring slack events api

--- server/services/slack/index.js
+++ server/services/slack/index.js
@@ -16,6 +16,10 @@ const { WebClient } = require('@slack/client')
 const { createMessageAdapter } = require('@slack/interactive-messages')
 const slackInteractions = createMessageAdapter(process.env.SLACK_SIGNING_SECRET)
 
+const { createEventAdapter } = require('@slack/events-api')
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
+
+
 const token = process.env.SLACK_BOT_OAUTH_ACCESS_TOKEN
 
 const web = new WebClient(token)
@@ -73,7 +77,19 @@ const getUsers = async () => {
   }
 }
 
+slackEvents.on('team_join', (event) => {
+  try {
+    console.log('TEAM JOIN EVENT => ', event)
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+slackEvents.on('error', console.error)
+
 router.use('/actions', slackInteractions.expressMiddleware())
+
+router.use('/events', slackEvents.expressMiddleware())
 
 module.exports = { router, web, sendMessage, getUsers }
 
